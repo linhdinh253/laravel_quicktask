b55b073cdcab1de797634fd42b56e10c01e07d5c
linhdinh253 <linhdinh253@gmail.com>
2020-09-30 10:17:08 +0700

    Added migration and model

--- database/migrations/2020_09_29_064619_create_tasks_table.php
+++ database/migrations/2020_09_29_064619_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('fname');
-            $table->string('lname');            
-            $table->string('email')->unique();
-            $table->string('password');
             $table->timestamps();
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+
         });
     }
 
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 }
